903cca3a0254f60fedf6435a08659fdec634d0e9
layarajank <58158380+layarajank@users.noreply.github.com>
2019-12-11 18:01:31 +0530

    Add files via upload

---
+++ Autoboxing.java
@@ -0,0 +1,12 @@
+package wrapper;
+
+public class Autoboxing {
+	public static void main(String args[])
+	{
+		Integer i=20;
+		Integer j=i;
+		System.out.println(i+ " "+j);
+	}
+	
+
+}
---
+++ Unboxing.java
@@ -0,0 +1,14 @@
+package wrapper;
+
+public class Unboxing {
+	public static void main(String args[])
+	{
+		Integer a=new Integer(10);
+		int i=a.intValue();
+		int j=i;
+		System.out.println(a+" "+i+" "+j);
+	}
+	}
+	
+
+
