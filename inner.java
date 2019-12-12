e39e07bd0968d86eb5294016dbd99fb8d3c31d12
layarajank <58158380+layarajank@users.noreply.github.com>
2019-12-12 14:32:33 +0530

    Example program for inner class

--- inner.java
+++ inner.java
@@ -1,19 +1,21 @@
 package New;
-
-public class inner {
+//outer class
+public class Inner 
+{
 	private int a=20;
-	class innerclass
+	//inner class
+	class Innerclass
 	{
 		public void display()
 		{
-		System.out.println(a);
+		    System.out.println(a); //acces private variable of outer class
 		}
 	}
 	public static void main(String args[])
 	{
-	inner obj=new inner();
-	inner.innerclass obj2=obj.new innerclass();
-	obj2.display();
+	     Inner obj=new Inner(); //creates object for outer class
+	     Inner.Innerclass obj2=obj.new Innerclass(); //Creating object for inner class with the reference of outer class
+	     obj2.display();
 	}
 
 }
