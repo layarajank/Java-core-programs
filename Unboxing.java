2b3b62e1863bdab24660839bad51351f8e931dc4
layarajank <58158380+layarajank@users.noreply.github.com>
2019-12-12 12:33:36 +0530

    Example program for Unboxing in java

--- Unboxing.java
+++ Unboxing.java
@@ -1,14 +1,15 @@
 package wrapper;
 
-public class Unboxing {
+public class Unboxing
+{
 	public static void main(String args[])
 	{
 		Integer a=new Integer(10);
-		int i=a.intValue();
-		int j=i;
+		int i=a.intValue(); // convert integer to int
+		int j=i; //unboxing
 		System.out.println(a+" "+i+" "+j);
 	}
-	}
+}
 	
 
 
